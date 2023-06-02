9cb0f1d0e4063a9a16700f07fcba418a74ff5464
Michal <michal.erik.czajkowski@gmail.com>
2023-06-02 21:34:12 +0200

    handled adding and displaying user name and todo list with local storage

--- main.js
+++ main.js
@@ -1,7 +1,67 @@
 const userNameInput = document.querySelector('#name');
+const newTodoForm = document.querySelector('#new-todo-form');
+const newTodoInput = document.querySelector('#content');
+const todoList = document.querySelector('#todo-list');
+const allTodos = JSON.parse(localStorage.getItem('allTodos')) || [];
+
+const displayAllTodos = (allTodos) => {
+  todoList.innerHTML = '';
+  allTodos.forEach((todo) => {
+    console.log(todo);
+    const markup = `
+        <div class="todo-item">
+            <label class="todo-inline">
+                <label class="todo-checkbox">
+                    <input type="checkbox" />
+                    <span class="bubble ${todo.category}"></span>
+                </label>
+                <p class="todo-content">
+                ${todo.content}
+                </p>
+            </label>
+            <div class="actions">
+                <button class="edit">Edit</button>
+                <button class="delete">Delete</button>
+            </div>
+        </div>
+        `;
+
+    todoList.insertAdjacentHTML('afterbegin', markup);
+  });
+};
+
+const handleAddTodo = (event) => {
+  event.preventDefault();
+
+  const newTodo = {
+    content: event.target.elements.content.value,
+    category: event.target.elements.category.value,
+    done: false,
+    createdAt: new Date().getTime(),
+  };
+
+  console.log(newTodo);
+  if (!newTodo.content || !newTodo.category) {
+    alert('pls fill all inputs');
+    return;
+  }
+
+  allTodos.push(newTodo);
+  localStorage.setItem('allTodos', JSON.stringify(allTodos));
+
+  event.target.reset();
+  displayAllTodos(allTodos);
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 userNameInput.value = localStorage.getItem('userName') || '';
+displayAllTodos(allTodos);
 
 userNameInput.addEventListener('input', () => {
   localStorage.setItem('userName', userNameInput.value);
 });
+
+newTodoForm.addEventListener('submit', handleAddTodo);
